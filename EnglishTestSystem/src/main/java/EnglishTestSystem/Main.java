f04743874ba5b41eab361cc5b99fd60454b38d10
ngocphucnguyen256 <69982260+ngocphucnguyen256@users.noreply.github.com>
2021-01-12 17:46:26 +0700

    Main

--- EnglishTestSystem/src/main/java/EnglishTestSystem/Main.java
+++ EnglishTestSystem/src/main/java/EnglishTestSystem/Main.java
@@ -7,23 +7,13 @@ package EnglishTestSystem;
 import java.sql.Date;
 import java.util.Scanner;
 import java.sql.*;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.*;
 /**
  *
  * @author Admin
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        
-//        QuestionList list = DataBase.openGetData();
-//        System.out.println(list);
        Scanner s= new Scanner(System.in);
        //Nhap nguoi dung
        User studier;
@@ -33,87 +23,99 @@ public class Main {
        String inp;
        int choose;
         do{
-        System.out.println("Moi ban den voi he thong trac nghiem tieng anh sieu cap vjp pro\n"
-                + "0.Thoat\n1. Dang nhap\n"
-                + "2.Dang ki\n");
-        System.out.println("Moi ban chon: ");
-        choose=s.nextInt();
-        s.nextLine();
-        if(choose==1){
-            System.out.println("Nhap ten: ");
-            inp= s.nextLine();
-            studier = uList.find(inp);
-                if(studier != null){
-                    System.out.println("Dang nhap thanh cong\n");
-                    System.out.println(studier);
-                    break;}
-                else
-                    System.out.println("\nKhong tim thay ten trong he thong");}
-         if(choose==2){
-             
-             String name,home,gender;
-             System.out.println("Nhap ten");
-             name=s.nextLine();             
-             System.out.println("Nhap noi sinh");
-             home=s.nextLine();
-             System.out.println("Nhap gioi tinh");
-             gender=s.nextLine();
-             System.out.println("Nhap ngay thang nam sinh");
-             Date dateOfBirth = Date.valueOf(s.nextLine());
-             User u = new User(name, gender, home, dateOfBirth);
-             uList.addUser(u);
-             DataBase.openSetDataUser(u);
-         }
-         if(choose==0){
-             System.exit(0);
-         }
+            try{
+                System.out.println("******Moi ban den voi he thong trac nghiem "
+                        + "tieng anh*******\n"
+                        + "0.Thoat\n1.Dang nhap\n"
+                        + "2.Dang ki\n");
+                System.out.println(">Moi ban chon: ");
+                choose=s.nextInt();
+                s.nextLine();
+                if(choose==1){
+                    System.out.println(">Nhap ten: ");
+                    inp= s.nextLine();
+                    studier = uList.find(inp);
+                        if(studier != null){
+                            System.out.println("Dang nhap thanh cong\n");
+                            System.out.println(studier);
+                            break;}
+                        else
+                            System.out.println("\nKhong tim thay ten trong he thong");}
+                 if(choose==2){
+
+                     String name,home,gender;
+                     System.out.println(">Nhap ten");
+                     name=s.nextLine();             
+                     System.out.println(">Nhap noi sinh");
+                     home=s.nextLine();
+                     System.out.println(">Nhap gioi tinh");
+                     gender=s.nextLine();
+                     System.out.println(">Nhap ngay thang nam sinh");
+                     Date dateOfBirth = Date.valueOf(s.nextLine());
+                     User u = new User(name, gender, home, dateOfBirth);
+                     uList.addUser(u);
+                     DataBase.openSetDataUser(u);
+                 }
+                 if(choose==0){
+                     System.exit(0);
+                 }
+             }
+        catch(InputMismatchException ex){
+            System.out.println("Nhap sai!!!!!!!!!");}
         }
-    while(true);
-//    //Ket thuc nhap nguoi dung
-//
-//         QuestionList list = new QuestionList();
-//         list = DataBase.openGetData();
-//          System.out.println(list);
-//          studier.getPoint().add(9.5);
-//          System.out.println(studier);
-//          uList.upDateDB();
-       
-        QuestionList listData = DataBase.openGetData();
-  
-        System.out.println("\n--------Luyen tap-----------\n Moi chon dang cau hoi:"
-                + "\n1.Multiple choice\n 2.Incomplete \n3.Conversation\n Chon=: ");
-        choose= s.nextInt();
-        s.nextLine();
-        
-        if(choose==1)
-        {
-            QuestionList list = new QuestionList();
-            System.out.println("\nNhap so cau hoi:");
-            choose=s.nextInt();
-            s.nextLine();
-            list = listData.getMultiple();
-//            Collections.shuffle((List<?>) list); //Trao list
-            System.out.println(list.show(choose)); //Nhap cau tra loi xong xuat
-        }
-        else if(choose==2){
-            QuestionList list = new QuestionList();
-            System.out.println("\nNhap muc do cau hoi:");
-            choose=s.nextInt();
-            s.nextLine();
-            list = listData.getIncomplete().find(choose);
-            System.out.println(list.showIncomplete());
-            //xu li
-        }
-        else{
-            QuestionList list = new QuestionList();
-            System.out.println("\nNhap muc do cau hoi:");
-            choose=s.nextInt();
-            s.nextLine();
-            list = listData.getConversation().find(choose);
-            System.out.println(list.showConversation());
-            //xu li
-        }
- 
+        while(true);
+//Ket thuc nhap nguoi dung
+ do{
+       try{
+           System.out.println("******HE THONG TRAC NGHIEM TIENG ANH******\n 1.Luyen tap"
+                   + "\n2.Xem thong tin\n0.Thoat\n>Moi ban chon:");
+           choose=s.nextInt();
+           s.nextLine();
+           QuestionList listData = DataBase.openGetData();
+           QuestionList list = new QuestionList();
+           if(choose==1){
+                System.out.println("\n--------Luyen tap-----------\n Moi chon dang cau hoi:"
+                        + "\n1.Multiple choice\n 2.Incomplete \n3.Conversation\n>Chon: ");
+                choose= s.nextInt();
+                s.nextLine();
+                if(choose==1)
+                { 
+                    System.out.println("\n>Nhap so luong cau hoi ban muon lam:");
+                    choose=s.nextInt();
+                    s.nextLine();
+                    list = listData.getMultiple();
+                    System.out.println(list.show(choose));
+                }
+                else if(choose==2){
+                    Question q;
+                    System.out.println("\n>Nhap muc do cau hoi ban muon lam:"
+                            + "\n 1.De \n 2.Trung binh \n 3.Kho"
+                            + "\n>Chon:");
+                    choose=s.nextInt();
+                    s.nextLine();
+                    list = listData.getIncomplete().find(choose);
+                    System.out.println(list.showIncomplete());
+                }
+                else if(choose==3){
+                    Question q;
+                    System.out.println("\n>Nhap muc do cau hoi ban muon lam:"
+                            + "\n 1.De \n 2.Trung binh \n 3.Kho"
+                            + "\n>Chon:");
+                    choose=s.nextInt();
+                    s.nextLine();
+                    list = listData.getConversation().find(choose);
+                    System.out.println(list.showConversation());
+                }
+           }
+           else if(choose==2){
+               System.out.println("\n-------Thong tin hoc vien---------");
+               System.out.println(studier);
+           }
+           else if(choose==0){
+               System.exit(0);
+           }
+       }catch(InputMismatchException ex){
+           System.out.println("Nhap sai!!!!!!!!!");}
+  }while(true);
     }
-    
 }
